2d7dfca75e5e7353318ec9b135bd58c4c5b3d677
tdemo086 <tdemo086@gmail.com>
2024-08-27 08:00:03 +0600

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///posts.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static', 'uploads')
-  
+    
